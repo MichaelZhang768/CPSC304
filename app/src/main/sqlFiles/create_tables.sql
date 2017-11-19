e038a54a2385acc7656263a851aea8a6095eb4ef
Michael <michaelzhang768@gmail.com>
2017-11-19 00:20:35 -0800

    Fixed sql scripts

--- app/src/main/sqlFiles/create_tables.sql
+++ app/src/main/sqlFiles/create_tables.sql
@@ -1,6 +1,6 @@
 use sql3205841;
 
-create table User (
+create table Users (
 email varchar(30) not null,
 password varchar(16) not null,
 username varchar(16) not null,
@@ -10,21 +10,21 @@ PRIMARY KEY (username));
 create table AdminUser (
 username varchar(16) not null,
 PRIMARY KEY (username),
-foreign key (username) references User(username));
+foreign key (username) references Users(username));
 
 create table RegularUser (
 username varchar(16) not null,
 accountBalance float(32) not null,
 PRIMARY KEY (username),
-foreign key (username) references User(username));
+foreign key (username) references Users(username));
 
 create table Friend (
 username1 varchar(16) not null,
 username2 varchar(16) not null,
 dateOfFriendship date not null,
 PRIMARY KEY (username1, username2),
-foreign key (username1) references User(username),
-foreign key (username2) references User(username));
+foreign key (username1) references Users(username),
+foreign key (username2) references Users(username));
 
 create table Payment (
 username varchar(16) not null,
@@ -39,6 +39,14 @@ create table Developer (
 developerName varchar(32) not null,
 PRIMARY KEY (developerName));
 
+create table Game (
+gameId integer not null,
+currentPrice float(16) not null,
+developerName varchar(32) not null,
+gameName varchar(32) not null,
+PRIMARY KEY (gameId),
+foreign key (developerName) references Developer(developerName));
+
 create table Develops (
 developerName varchar(32) not null,
 gameId integer not null,
@@ -47,13 +55,6 @@ PRIMARY KEY (developerName, gameId),
 foreign key (developerName) references Developer(developerName),
 foreign key (gameId) references Game(gameId));
 
-create table Game (
-gameId integer not null,
-currentPrice float(16) not null,
-developerName varchar(32) not null,
-gameName varchar(32) not null,
-PRIMARY KEY (gameId),
-foreign key (developerId) references Developer(developerId));
 
 create table DLC (
 dlcId integer not null,
@@ -69,13 +70,13 @@ gameId integer not null,
 purchasePrice float(32) not null,
 dateOfPurchase date not null,
 PRIMARY KEY (username, gameId),
-foreign key (username) references User(username),
+foreign key (username) references Users(username),
 foreign key (gameId) references Game(gameId));
 
 create table IsGenre (
 gameId integer not null,
 genreName varchar(16) not null,
-PRIMARY KEY (genreName))
+PRIMARY KEY (genreName),
 foreign key (gameId) references Game(gameId));
 
 commit;
--- app/src/main/sqlFiles/insert_rows.sql
+++ app/src/main/sqlFiles/insert_rows.sql
@@ -1,134 +1,132 @@
-use cpsc304;
+insert into Users values("user1@gmail.com", "password1", "username1", "N/A");
 
-insert into User values("user1@gmail.com", "password1", "username1", "N/A");
+insert into Users values("user2@gmail.com", "password2", "username2", "N/A");
 
-insert into User values("user2@gmail.com", "password2", "username2", "N/A");
+insert into Users values("user3@gmail.com", "password3", "username3", "N/A");
 
-insert into User values("user3@gmail.com", "password3", "username3", "N/A");
+insert into Users values("user4@gmail.com", "password4", "username4", "N/A");
 
-insert into User values("user4@gmail.com", "password4", "username4", "N/A");
+insert into Users values("user5@gmail.com", "password5", "username5", "N/A");
 
-insert into User values("user5@gmail.com", "password5", "username5", "N/A");
+insert into Users values("user6@gmail.com", "password6", "username6", "N/A");
 
-insert into User values("user6@gmail.com", "password6", "username6", "N/A");
+insert into Users values("user7@gmail.com", "password7", "username7", "N/A");
 
-insert into User values("user7@gmail.com", "password7", "username7", "N/A");	
+insert into Users values("user8@gmail.com", "password8", "username8", "N/A");
 
-insert into User values("user8@gmail.com", "password8", "username8", "N/A");
+insert into Users values("user9@gmail.com", "password9", "username9", "N/A");
 
-insert into User values("user9@gmail.com", "password9", "username9", "N/A");
+insert into Users values("user10@gmail.com", "password10", "username10", "N/A");
 
-insert into User values("user10@gmail.com", "password10", "username10", "N/A");
+insert into Users values("user11@gmail.com", "password11", "username11", "N/A");
 
-insert into User values("user11@gmail.com", "password11", "username11", "N/A");
+insert into Users values("user12@gmail.com", "password12", "username12", "N/A");
 
-insert into User values("user12@gmail.com", "password12", "username12", "N/A");
+insert into Users values("user13@gmail.com", "password13", "username13", "N/A");
 
-insert into User values("user13@gmail.com", "password13", "username13", "N/A");
+insert into Users values("user14@gmail.com", "password14", "username14", "N/A");
 
-insert into User values("user14@gmail.com", "password14", "username14", "N/A");
+insert into Users values("user15@gmail.com", "password15", "username15", "N/A");
 
-insert into User values("user15@gmail.com", "password15", "username15", "N/A");
+insert into Users values("user16@gmail.com", "password16", "username16", "N/A");
 
-insert into User values("user16@gmail.com", "password16", "username16", "N/A");
+insert into Users values("user17@gmail.com", "password17", "username17", "N/A");
 
-insert into User values("user17@gmail.com", "password17", "username17", "N/A");
+insert into Users values("user18@gmail.com", "password18", "username18", "N/A");
 
-insert into User values("user18@gmail.com", "password18", "username18", "N/A");
+insert into Users values("user19@gmail.com", "password19", "username19", "N/A");
 
-insert into User values("user19@gmail.com", "password19", "username19", "N/A");
+insert into Users values("user20@gmail.com", "password20", "username20", "N/A");
 
-insert into User values("user20@gmail.com", "password20", "username20", "N/A");
+insert into AdminUser values("username1");
 
-insert into AdminUser values(username1);
+insert into AdminUser values("username2");
 
-insert into AdminUser values(username2);
+insert into AdminUser values("username3");
 
-insert into AdminUser values(username3);
+insert into AdminUser values("username4");
 
-insert into AdminUser values(username4);
+insert into AdminUser values("username5");
 
-insert into AdminUser values(username5);
+insert into AdminUser values("username6");
 
-insert into AdminUser values(username6);
+insert into AdminUser values("username7");
 
-insert into AdminUser values(username7);
+insert into AdminUser values("username8");
 
-insert into AdminUser values(username8);
+insert into AdminUser values("username9");
 
-insert into AdminUser values(username9);
+insert into AdminUser values("username10");
 
-insert into AdminUser values(username10);
+insert into RegularUser values("username11", 100.00);
 
-insert into RegularUser values(username2, 100.00);
+insert into RegularUser values("username12", 53.24);
 
-insert into RegularUser values(username3, 53.24);
+insert into RegularUser values("username13", 2342.75);
 
-insert into RegularUser values(username4, 2342.75);
+insert into RegularUser values("username14", 24.00);
 
-insert into RegularUser values(username5, 24.00);
-
-insert into RegularUser values(username6, 0.00);
+insert into RegularUser values("username15", 0.00);
 	
-insert into RegularUser values(username7, 245.84);
+insert into RegularUser values("username16", 245.84);
 
-insert into RegularUser values(username8, 0.00);
+insert into RegularUser values("username17", 0.00);
 
-insert into RegularUser values(username9, 60.00);
+insert into RegularUser values("username18", 60.00);
 
-insert into RegularUser values(username20, 153.45);
+insert into RegularUser values("username19", 153.45);
 
-insert into RegularUser values(username21, 153512335.52);
+insert into RegularUser values("username20", 153512335.52);
 
-insert into Friend values(username1, username2, '2017-1-10');
-insert into Friend values(username2, username1, '2017-1-10');
+insert into Friend values("username1", "username2", '2017-1-10');
+insert into Friend values("username2", "username1", '2017-1-10');
 
-insert into Friend values(username1, username3, '2017-2-10');
-insert into Friend values(username3, username1, '2017-2-10');
+insert into Friend values("username1", "username3", '2017-2-10');
+insert into Friend values("username3", "username1", '2017-2-10');
 
-insert into Friend values(username1, username4, '2017-3-10');
-insert into Friend values(username4, username1, '2017-3-10');
+insert into Friend values("username1", "username4", '2017-3-10');
+insert into Friend values("username4", "username1", '2017-3-10');
 
-insert into Friend values(username4, username5, '2017-4-10');
-insert into Friend values(username5, username4, '2017-4-10');
+insert into Friend values("username4", "username5", '2017-4-10');
+insert into Friend values("username5", "username4", '2017-4-10');
 
-insert into Friend values(username2, username3, '2017-5-10');
-insert into Friend values(username3, username2, '2017-5-10');
+insert into Friend values("username2", "username3", '2017-5-10');
+insert into Friend values("username3", "username2", '2017-5-10');
 
-insert into Friend values(username1, username5, '2017-6-10');
-insert into Friend values(username5, username1, '2017-6-10');
+insert into Friend values("username1", "username5", '2017-6-10');
+insert into Friend values("username5", "username1", '2017-6-10');
 
-insert into Friend values(username5, username21, '2017-7-10');
-insert into Friend values(username21, username5, '2017-7-10');
+insert into Friend values("username5", "username20", '2017-7-10');
+insert into Friend values("username20", "username5", '2017-7-10');
 
-insert into Friend values(username6, username7, '2017-8-10');
-insert into Friend values(username7, username6, '2017-8-10');
+insert into Friend values("username6", "username7", '2017-8-10');
+insert into Friend values("username7", "username6", '2017-8-10');
 
-insert into Friend values(username5, username6, '2017-9-10');
-insert into Friend values(username6, username5, '2017-9-10');
+insert into Friend values("username5", "username6", '2017-9-10');
+insert into Friend values("username6", "username5", '2017-9-10');
 
-insert into Friend values(username6, username9, '2017-10-10');
-insert into Friend values(username9, username6, '2017-10-10');
+insert into Friend values("username6", "username9", '2017-10-10');
+insert into Friend values("username9", "username6", '2017-10-10');
 
-insert into Payment values(username2, 00000000, '2017-11-10', 128.00, "Credit Card");
+insert into Payment values("username12", 0, '2017-11-10', 128.00, "Credit Card");
 
-insert into Payment values(username2, 00000002, '2017-11-09', 99.00, "PayPal");
+insert into Payment values("username12", 2, '2017-11-09', 99.00, "PayPal");
 
-insert into Payment values(username3, 00000008, '2001-09-11', 39.00, "Debit Card");
+insert into Payment values("username13", 8, '2001-09-11', 39.00, "Debit Card");
 
-insert into Payment values(username3, 00000009, '2017-11-09', 49.00, "PayPal");
+insert into Payment values("username13", 9, '2017-11-09', 49.00, "PayPal");
 
-insert into Payment values(username4, 00000003, '2017-11-09', 29.00, "Credit Card");
+insert into Payment values("username14", 3, '2017-11-09', 29.00, "Credit Card");
 
-insert into Payment values(username4, 00000004, '2017-05-23', 167.00, "Account Credit");
+insert into Payment values("username14", 4, '2017-05-23', 167.00, "Account Credit");
 
-insert into Payment values(username5, 00000005, '2017-05-31', 29.00, "Gift Card");
+insert into Payment values("username15", 5, '2017-05-31', 29.00, "Gift Card");
 
-insert into Payment values(username6, 00000006, '2017-02-09', 19.00, "Debit Card");
+insert into Payment values("username16", 6, '2017-02-09', 19.00, "Debit Card");
 
-insert into Payment values(username7, 00000007, '2017-10-23', 29.00, "Credit Card");
+insert into Payment values("username17", 7, '2017-10-23', 29.00, "Credit Card");
 
-insert into Payment values(username2, 00000001, '2017-09-09', 0.00, "Instant Noodles");
+insert into Payment values("username12", 1, '2017-09-09', 0.00, "Instant Noodles");
 
 insert into Developer values("Electronic Smarts");
 
@@ -150,104 +148,104 @@ insert into Developer values("Deactivision");
 
 insert into Developer values("Mediocre Games");
 
-insert into Develops values("Electronic Smarts", 0000000, '2017-01-09');
+insert into Game values(0, 59.00, "Electronic Smarts", "Connect 4");
 
-insert into Develops values("Electronic Smarts", 0000001, '2017-01-09');
+insert into Game values(1, 69.00, "Electronic Smarts", "Connect 5");
 
-insert into Develops values("Many Thinking Apes", 0000002, '2017-02-09');
+insert into Game values(2, 29.00, "Many Thinking Apes", "Party In My Basement");
 
-insert into Develops values("Macrosoft Studios", 0000003, '2017-03-09');
+insert into Game values(3, 19.00, "Macrosoft Studios", "Potato Ninja");
 
-insert into Develops values("Rito Games", 0000004, '2017-04-09');
+insert into Game values(4, 39.00, "Rito Games", "Legends of League");
 
-insert into Develops values("Valve", 0000005, '2017-05-09');
+insert into Game values(5, 59.00, "Valve", "The Lab");
 
-insert into Develops values("Valve", 0000006, '2017-06-09');
+insert into Game values(6, 59.00, "Valve", "The Potato");
 
-insert into Develops values("Fish Games", 0000007, '2017-07-09');
+insert into Game values(7, 49.00, "Fish Games", "Feed the Fish");
 
-insert into Develops values("MICE", 0000008, '2017-05-09');
+insert into Game values(8, 89.00, "MICE", "Micefield 1");
 
-insert into Develops values("Deactivision", 0000009, '2017-01-09');
+insert into Game values(9, 99.00, "Deactivision", "Call of Potatoes");
 
-insert into Game values(0000000, 59.00, "Electronic Smarts", "Connect 4");
+insert into Develops values("Electronic Smarts", 0, '2017-01-09');
 
-insert into Game values(0000001, 69.00, "Electronic Smarts", "Connect 5");
+insert into Develops values("Electronic Smarts", 1, '2017-01-09');
 
-insert into Game values(0000002, 29.00, "Many Thinking Apes", "Party In My Basement");
+insert into Develops values("Many Thinking Apes", 2, '2017-02-09');
 
-insert into Game values(0000003, 19.00, "Macrosoft Studios", "Potato Ninja");
+insert into Develops values("Macrosoft Studios", 3, '2017-03-09');
 
-insert into Game values(0000004, 39.00, "Rito Games", "Legends of League");
+insert into Develops values("Rito Games", 4, '2017-04-09');
 
-insert into Game values(0000005, 59.00, "Valve", "The Lab");
+insert into Develops values("Valve", 5, '2017-05-09');
 
-insert into Game values(0000006, 59.00, "Valve", "The Potato");
+insert into Develops values("Valve", 6, '2017-06-09');
 
-insert into Game values(0000007, 49.00, "Fish Games", "Feed the Fish");
+insert into Develops values("Fish Games", 7, '2017-07-09');
 
-insert into Game values(0000008, 89.00, "MICE", "Micefield 1");
+insert into Develops values("MICE", 8, '2017-05-09');
 
-insert into Game values(0000009, 99.00, "Deactivision", "Call of Potatoes");
+insert into Develops values("Deactivision", 9, '2017-01-09');
 
-insert into IsGenre values(00000000, "MOBA");
+insert into IsGenre values(0, "MOBA");
 
-insert into IsGenre values(00000001, "FPS");
+insert into IsGenre values(1, "FPS");
 
-insert into IsGenre values(00000002, "VR");
+insert into IsGenre values(2, "VR");
 
-insert into IsGenre values(00000003, "Fun");
+insert into IsGenre values(3, "Fun");
 
-insert into IsGenre values(00000004, "RPG");
+insert into IsGenre values(4, "RPG");
 
-insert into IsGenre values(00000005, "Adventure");
+insert into IsGenre values(5, "Adventure");
 
-insert into IsGenre values(00000006, "Survival");
+insert into IsGenre values(6, "Survival");
 
-insert into IsGenre values(00000007, "Strategy");
+insert into IsGenre values(7, "Strategy");
 
-insert into IsGenre values(00000008, "Fantasy");
+insert into IsGenre values(8, "Fantasy");
 
-insert into IsGenre values(00000009, "Horror");
+insert into IsGenre values(9, "Horror");
 
-insert into Owns values(username2, 00000000, 59.00, '2017-11-10');
+insert into Owns values("username12", 0, 59.00, '2017-11-10');
 
-insert into Owns values(username2, 00000001, 69.00, '2017-11-05');
+insert into Owns values("username12", 1, 69.00, '2017-11-05');
 
-insert into Owns values(username3, 00000000, 59.00, '2015-11-10');
+insert into Owns values("username13", 0, 59.00, '2015-11-10');
 
-insert into Owns values(username4, 00000003, 19.00, '2013-09-11');
+insert into Owns values("username14", 3, 19.00, '2013-09-11');
 
-insert into Owns values(username5, 00000006, 59.00, '2013-12-04');
+insert into Owns values("username15", 6, 59.00, '2013-12-04');
 
-insert into Owns values(username6, 00000005, 59.00, '2016-07-29');
+insert into Owns values("username16", 5, 59.00, '2016-07-29');
 
-insert into Owns values(username7, 00000009, 99.00, '2017-11-01');
+insert into Owns values("username17", 9, 99.00, '2017-11-01');
 
-insert into Owns values(username8, 00000006, 59.00, '2017-02-11');
+insert into Owns values("username18", 6, 59.00, '2017-02-11');
 
-insert into Owns values(username9, 00000007, 79.00, '2001-11-10');
+insert into Owns values("username19", 7, 79.00, '2001-11-10');
 
-insert into Owns values(username10, 00000009, 99.00, '2010-02-25');
+insert into Owns values("username11", 9, 99.00, '2010-02-25');
 
-insert into DLC values(00000000, 00000004, "Summoner's Canyon", 19.00);
+insert into DLC values(0, 4, "Summoner's Canyon", 19.00);
 
-insert into DLC values(00000001, 00000002, "Alcohol Expansion", 19.00);
+insert into DLC values(1, 2, "Alcohol Expansion", 19.00);
 
-insert into DLC values(00000002, 00000005, "For Science!", 19.00);
+insert into DLC values(2, 5, "For Science!", 19.00);
 
-insert into DLC values(00000003, 00000008, "French Stuff", 19.00);
+insert into DLC values(3, 8, "French Stuff", 19.00);
 
-insert into DLC values(00000004, 00000004, "New Champinos", 19.00);
+insert into DLC values(4, 4, "New Champinos", 19.00);
 
-insert into DLC values(00000005, 00000009, "Tomatoes Invade", 19.00);
+insert into DLC values(5, 9, "Tomatoes Invade", 19.00);
 
-insert into DLC values(00000006, 00000008, "French Mice", 29.00);
+insert into DLC values(6, 8, "French Mice", 29.00);
 
-insert into DLC values(00000007, 00000006, "Free Stuff", 0.00);
+insert into DLC values(7, 6, "Free Stuff", 0.00);
 
-insert into DLC values(00000008, 00000003, "Tomato Ninja", 19.00);
+insert into DLC values(8, 3, "Tomato Ninja", 19.00);
 
-insert into DLC values(00000009, 00000008, "Russian Mice", 19.00);
+insert into DLC values(9, 8, "Russian Mice", 19.00);
 
 commit work;

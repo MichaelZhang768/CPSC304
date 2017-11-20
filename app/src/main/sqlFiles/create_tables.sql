5538f72dcf6171d1f11c9eeced905cb144f8d8da
Yan <ibyanli@gmail.com>
2017-11-19 22:53:35 -0800

    table edits

--- app/src/main/sqlFiles/create_tables.sql
+++ app/src/main/sqlFiles/create_tables.sql
@@ -1,19 +1,19 @@
 use sql3205841;
 
 create table Users (
-email varchar(30) not null,
+email varchar(32) not null,
 password varchar(16) not null,
-username varchar(16) not null,
+username varchar(32) not null,
 description varchar(16) not null,
 PRIMARY KEY (username));
 
 create table AdminUser (
-username varchar(16) not null,
+username varchar(32) not null,
 PRIMARY KEY (username),
 foreign key (username) references Users(username));
 
 create table RegularUser (
-username varchar(16) not null,
+username varchar(32) not null,
 accountBalance float(32) not null,
 PRIMARY KEY (username),
 foreign key (username) references Users(username));
@@ -27,7 +27,7 @@ foreign key (username1) references Users(username),
 foreign key (username2) references Users(username));
 
 create table Payment (
-username varchar(16) not null,
+username varchar(32) not null,
 paymentNumber integer not null,
 dateOfPayment date not null,
 cost float(16) not null,
@@ -37,12 +37,12 @@ foreign key (username) references RegularUser(username));
 
 create table Developer (
 developerName varchar(32) not null,
-PRIMARY KEY (developerName)
+PRIMARY KEY (developerName),
 foreign key (developerName) references Users(username));
 
 create table Game (
 gameId integer not null,
-currentPrice float(16) not null,
+currentPrice float(32) not null,
 developerName varchar(32) not null,
 gameName varchar(32) not null,
 PRIMARY KEY (gameId),
@@ -56,17 +56,16 @@ PRIMARY KEY (developerName, gameId),
 foreign key (developerName) references Developer(developerName),
 foreign key (gameId) references Game(gameId));
 
-
 create table DLC (
 dlcId integer not null,
 gameId integer not null,
-dlcName varchar(16) not null,
+dlcName varchar(32) not null,
 dlcPrice float(32) not null,
 PRIMARY KEY (dlcId, gameId),
 foreign key (gameId) references Game(gameId) ON DELETE CASCADE);
 
 create table Owns (
-username varchar(16) not null,
+username varchar(32) not null,
 gameId integer not null,
 purchasePrice float(32) not null,
 dateOfPurchase date not null,
